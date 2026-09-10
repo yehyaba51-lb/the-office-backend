e529e324b121af91fc257171b9562c8e46970960
yehyaba51-lb <yehyaba51@gmail.com>
2026-09-10 23:53:04 +0200

    feat: add getProgressionByLeconEtudiant and updateProgressionLecon

--- classes/acces_donnees/ProgressionLeconModel.php
+++ classes/acces_donnees/ProgressionLeconModel.php
@@ -62,7 +62,9 @@
 
 
         public function creerProgressionLecon($data){
-            $stmt = mysqli_prepare($this->conn, "INSERT INTO progression_lecon(cours_id, lecon_id, etudiant_id, complete_le) VALUES(?, ?, ?, ?)");
+            $stmt = mysqli_prepare($this->conn, 
+                "INSERT INTO progression_lecon(cours_id, lecon_id, etudiant_id, complete_le)
+                VALUES(?, ?, ?, ?)");
 
             if (!$stmt) {
                 error_log('Prepare failed: ' . mysqli_error($this->conn));
@@ -73,5 +75,24 @@
             mysqli_stmt_execute($stmt);
 
             return mysqli_insert_id($this->conn);
+        }
+
+        public function updateProgressionLecon($data){
+            $stmt = mysqli_prepare($this->conn, 
+                "UPDATE progression_lecon
+                SET statut = ?,
+                complete_le = ?
+                WHERE etudiant_id = ?
+                AND cours_id = ?
+                AND lecon_id = ?"
+            );
+
+            if(!$stmt){
+                error_log('Prepare failed' . mysqli_error($this->conn));
+                return false;
+            }
+
+            mysqli_stmt_bind_param($stmt, "ssiii", $data['statut'], $data['complete_le'], $data['etudiant_id'], $data['cours_id'], $data['lecon_id']);
+            return mysqli_stmt_execute($stmt);
         }
     }
