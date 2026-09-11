7d31e4a131dbe8c73a1e2a7174b4d3ef53f0c97c
yehyaba51-lb <yehyaba51@gmail.com>
2026-09-11 02:44:09 +0200

    feat: scaffold FichierValidateur class structure

---
+++ classes/support/FichierValidateur.php
@@ -0,0 +1,16 @@
+<?php
+    class FichierValidateur{
+        private $TAILLE_MAX;
+
+        public function valider(){
+
+        }
+
+        public function getMimeType(){
+
+        }
+
+        public function genererNomFichier(){
+
+        }
+    }
