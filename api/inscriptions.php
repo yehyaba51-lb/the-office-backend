7c532a1e1022b40b62af766d4a90df620470d6e7
yehyaba51-lb <yehyaba51@gmail.com>
2026-09-13 01:37:28 +0200

    feat: add validation and fix argument passing for cours and inscriptions endpoints

--- api/inscriptions.php
+++ api/inscriptions.php
@@ -4,13 +4,18 @@
     $dotenv->load();
 
     header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type');
     header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
     require_once(__DIR__ . '/../classes/gestion_cours/CoursManager.php');
 
     $manager = new CoursManager();
 
-    if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit;
+    } else if($_SERVER['REQUEST_METHOD'] === 'GET'){
         $allInscriptions = $manager->getAllInscriptions();
 
         if(!$allInscriptions){
@@ -21,6 +26,36 @@
 
         http_response_code(200);
         echo json_encode($allInscriptions);
+    } else if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        $result = $manager->createInscription($data['etudiant_id'], $data['cours_id']);
+
+        if(!$result){
+            http_response_code(400);
+            echo json_encode(['error' => 'Inscription ajouté invalide']);
+            exit;
+        }
+
+        http_response_code(201);
+        echo json_encode($result);
+    }  else if($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+        if(!isset($_GET['id'])){
+            http_response_code(400);
+            echo json_encode(['error' => 'Id manquant']);
+            exit;
+        } else {
+            $delete = $manager->supprimerInscription($_GET['id']);
+
+            if(!$delete){
+                http_response_code(400);
+                echo json_encode(['error' => "Impossible de supprimer l'inscription"]);
+                exit;
+            }
+
+            http_response_code(200);
+            echo json_encode($delete);
+        }
     } else {
         http_response_code(405);
         echo json_encode(['error' => 'Méthode non autorisée']);
