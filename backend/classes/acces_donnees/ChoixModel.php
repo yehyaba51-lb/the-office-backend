453bf661f97b5a1426028985f30ff06728f1765d
yehyaba51-lb <yehyaba51@gmail.com>
2026-09-26 23:32:20 +0200

    feat: add getChoixPerLecon query

--- backend/classes/acces_donnees/ChoixModel.php
+++ backend/classes/acces_donnees/ChoixModel.php
@@ -92,6 +92,36 @@
             return mysqli_fetch_all($result, MYSQLI_ASSOC);
         }
 
+        public function getChoixPerLecon($cours_id, $lecon_id){
+            $stmt = mysqli_prepare($this->conn,
+                "SELECT *
+                FROM choix AS ch
+                INNER JOIN question AS q
+                ON ch.question_id = q.question_id
+                INNER JOIN exercice AS e
+                ON q.exercice_id = e.exercice_id
+                WHERE e.cours_id = ?
+                AND e.lecon_id = ?"
+            );
+
+            if(!$stmt){
+                error_log('Prepare failed: ' . mysqli_error($this->conn));
+                return false;
+            }
+
+            mysqli_stmt_bind_param($stmt, "ii", $cours_id, $lecon_id);
+
+            $execute = mysqli_stmt_execute($stmt);
+
+            if($execute === false){
+                return false;
+            }
+
+            $result = mysqli_stmt_get_result($stmt);
+
+            return mysqli_fetch_all($result, MYSQLI_ASSOC);
+        }
+
         public function creerChoix($data){
             if(empty($data['texte_choix']) || strlen(trim($data['texte_choix'])) < 2 || !preg_match("/^[a-zA-ZÀ-ÿ0-9' :\-]*$/u", $data['texte_choix'])){
                 return ['error' => 'Contenu invalide'];
